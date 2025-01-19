97b0032dd5d2fd20acb3a0d7f30e15480a851007
Valёk <uptodateman@proton.me>
2025-01-19 20:38:53 +0400

    chore: удалены неиспользуемые файлы
    - Удалены файлы конфигурации бота
    - Удалены временные файлы
    - Удалены компоненты старой версии приложения
    - Удален Dockerfile

--- src/config.js
+++
@@ -1,9 +0,0 @@
-// Конфигурация для фронтенда
-export const botConfig = {
-  botName: 'goods_track_bot',
-  size: 'large',
-  radius: '8',
-  // Добавляем язык и домен
-  lang: 'ru',
-  domain: 'https://robertuptodateman.github.io'
-}
