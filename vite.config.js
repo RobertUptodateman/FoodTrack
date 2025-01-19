a31528288b8e75c35cfa4e4762724c868e96f8e0
Valёk <uptodateman@proton.me>
2025-01-19 18:04:23 +0400

    ci: обновление workflow для ветки feature/js-processing

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: '/FoodTrack/' // для GitHub Pages
+  base: '/FoodTrack/'
 })
